6afa8671379ca59f2aa8b7bf96154499041c7fff
hdmun <hdmun@kakao.com>
2023-06-22 10:52:14 +0900

    REFACTOR: `XlsxDiff3Core.Run` 코드 정리

--- src/XlsxMerge/Diff/XlsxDiff3Core.cs
+++ src/XlsxMerge/Diff/XlsxDiff3Core.cs
@@ -1,5 +1,4 @@
-﻿using System.Text.RegularExpressions;
-using XlsxMerge.Diff;
+﻿using XlsxMerge.Diff;
 using XlsxMerge.Features;
 using XlsxMerge.Features.Diffs;
 using XlsxMerge.Features.Excels;
@@ -13,35 +12,16 @@ namespace XlsxMerge
 
         public List<SheetDiffResult> Run(PathViewModel pathViewModel)
         {
-			// 엑셀 파일을 해석.
-			using (var excelReader = new ExcelReader())
-            {
-                ParsedWorkbookMap[DocOrigin.Base] = excelReader.Read(pathViewModel.BasePath);
-                ParsedWorkbookMap[DocOrigin.Mine] = excelReader.Read(pathViewModel.MinePath);
-                if (pathViewModel.ComparisonMode == ComparisonMode.ThreeWay)
-                    ParsedWorkbookMap[DocOrigin.Theirs] = excelReader.Read(pathViewModel.TheirsPath);
-            }
+            ReadExcelFiles(pathViewModel.BasePath, pathViewModel.MinePath, pathViewModel.TheirsPath, pathViewModel.ComparisonMode);
 
-	        FakeBackgroundWorker.OnUpdateProgress("xlsx 파일 비교  [3단계 중 3단계]", "엑셀 문서 비교 중..");
-            // diff3 의 file1/file2/file3 셋업.
-            // 순서는 base/mine/theirs 이며, two-way는 theirs 위치에 base 문서를 넣는다.
-            // 이렇게 하면 two-way에서 diff3 hunk status 가 Diff3HunkStatus.MineDiffers 로 표시된다.
-
-            var xlsxList = new List<ExcelFile>
-            {
-                ParsedWorkbookMap[DocOrigin.Base],
-                ParsedWorkbookMap[DocOrigin.Mine],
-                pathViewModel.ComparisonMode switch
-                {
-                    ComparisonMode.ThreeWay => ParsedWorkbookMap[DocOrigin.Theirs],
-                    _ => ParsedWorkbookMap[DocOrigin.Base]
-                }
-            };
+            FakeBackgroundWorker.OnUpdateProgress("xlsx 파일 비교  [3단계 중 3단계]", "엑셀 문서 비교 중..");
 
             // 비교 대상 워크시트 목록을 추출
-            var sheetNameSet = xlsxList
+            var sheetNameSet = ParsedWorkbookMap.Values
                 .SelectMany(x => x.Worksheets.Select(y => y.Name))
                 .ToHashSet();
+
+            var docOrigins = Enum.GetValues<DocOrigin>();
 
             // 각 워크시트를 List<String>으로 변환 후 do diff3
             var compareResults = new List<SheetDiffResult>();
@@ -49,27 +29,42 @@ namespace XlsxMerge
             {
                 SheetDiffResult newSheetResult = new SheetDiffResult(worksheetName, pathViewModel.ComparisonMode);
 
-				string diff3ResultText = null;
+                var textLinesByOrigin = docOrigins.ToDictionary(
+                    x => x,
+                    x => ParsedWorkbookMap[x].GetTextLinesByWorksheetName(worksheetName)
+                );
+                foreach (var (docOrigin, textLine) in textLinesByOrigin.Where(x => x.Value != null))
                 {
-                    var lines1 = xlsxList[0].GetTextLinesByWorksheetName(worksheetName);
-                    var lines2 = xlsxList[1].GetTextLinesByWorksheetName(worksheetName);
-                    var lines3 = xlsxList[2].GetTextLinesByWorksheetName(worksheetName);
-
-                    if (lines1 != null)
-                        newSheetResult.DocsContaining.Add(DocOrigin.Base);
-                    if (lines2 != null)
-                        newSheetResult.DocsContaining.Add(DocOrigin.Mine);
-                    if (lines3 != null)
-                        newSheetResult.DocsContaining.Add(DocOrigin.Theirs);
-
-                    diff3ResultText = LaunchExternalDiff3Process(lines1, lines2, lines3);
+                    newSheetResult.DocsContaining.Add(docOrigin);
                 }
 
+                var baseTextLines = textLinesByOrigin[DocOrigin.Base];
+                var mineTextLines = textLinesByOrigin[DocOrigin.Mine];
+                var theirsTextLines = textLinesByOrigin[DocOrigin.Theirs];
+                string diff3ResultText = LaunchExternalDiff3Process(baseTextLines, mineTextLines, theirsTextLines);
+ 
                 newSheetResult.HunkList = new Diff3Parser().Parse(diff3ResultText);
 
                 compareResults.Add(newSheetResult);
             }
             return compareResults;
+        }
+
+        private void ReadExcelFiles(string basePath, string minePath, string theirsPath, ComparisonMode comparison)
+        {
+            // 엑셀 파일을 해석.
+            // diff3 의 file1/file2/file3 셋업.
+            // 순서는 base/mine/theirs 이며, two-way는 theirs 위치에 base 문서를 넣는다.
+            // 이렇게 하면 two-way에서 diff3 hunk status 가 Diff3HunkStatus.MineDiffers 로 표시된다.
+            using (var excelReader = new ExcelReader())
+            {
+                ParsedWorkbookMap[DocOrigin.Base] = excelReader.Read(basePath);
+                ParsedWorkbookMap[DocOrigin.Mine] = excelReader.Read(minePath);
+                if (comparison == ComparisonMode.ThreeWay)
+                    ParsedWorkbookMap[DocOrigin.Theirs] = excelReader.Read(theirsPath);
+                else
+                    ParsedWorkbookMap[DocOrigin.Theirs] = ParsedWorkbookMap[DocOrigin.Base];
+            }
         }
 
 	    public Dictionary<DocOrigin, ExcelWorksheet?> GetParsedWorksheetData(string worksheetName)
